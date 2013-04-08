f656d5976ee11c0280f016ef78cf9d4e7aa240e6
Jakub Ruzicka <jruzicka@redhat.com>
2013-04-08 20:24:29 +0200

    Also look for -update-candidate in koji.

--- verwatch/fetchers/koji.py
+++ verwatch/fetchers/koji.py
@@ -6,7 +6,7 @@ import re
 class KojiFetcher(VersionFetcher):
     name = 'koji'
 
-    def get_version(self, pkg_name, branch):
+    def _get_version(self, pkg_name, branch):
         cmd = "koji latest-pkg \"%s\" \"%s\"" % (branch, pkg_name)
         errc, out, err = run(cmd)
         if errc:
@@ -21,3 +21,11 @@ class KojiFetcher(VersionFetcher):
         if len(cols) == 1:
             return {'error': 'Koji output parsing failed: %s' % line}
         return parse_nvr(cols[0], pkg_name)
+
+    def get_version(self, pkg_name, branch):
+        ver = self._get_version(pkg_name, branch)
+        next_branch = "%s-updates-candidate" % branch
+        next_ver = self._get_version(pkg_name, next_branch)
+        if 'version' in next_ver:
+            ver['next'] = next_ver
+        return ver
