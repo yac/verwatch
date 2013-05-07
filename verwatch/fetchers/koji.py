7e59a4c7d2f1eae21aff88a95a06a99c687b64b8
Jakub Ruzicka <jruzicka@redhat.com>
2013-05-07 17:42:07 +0200

    Add brew support to koji fetcher.
    "options": {
        "command": "brew"
    }

--- verwatch/fetchers/koji.py
+++ verwatch/fetchers/koji.py
@@ -6,8 +6,16 @@ import re
 class KojiFetcher(VersionFetcher):
     name = 'koji'
 
+    def __init__(self, **kwargs):
+        VersionFetcher.__init__(self, **kwargs)
+        self.command = 'koji'
+        if 'options' in kwargs:
+            options = kwargs['options']
+            if 'command' in options:
+                self.command = options['command']
+
     def _koji_get_version(self, pkg_name, branch):
-        cmd = "koji latest-pkg \"%s\" \"%s\"" % (branch, pkg_name)
+        cmd = "%s latest-pkg \"%s\" \"%s\"" % (self.command, branch, pkg_name)
         errc, out, err = run(cmd)
         if errc:
             return {'error': err or out}
@@ -16,16 +24,21 @@ class KojiFetcher(VersionFetcher):
             return {'error': 'No results. Bad package name?'}
         if len(lines) > 1:
             return {'error':
-                    "`koji latest-pkg` returned more than one result. WAT?"}
+                    "`%s latest-pkg` returned more than one result. WAT?"
+                    % self.command}
         line = lines[0]
         cols = re.split(' +', line)
         if len(cols) == 1:
-            return {'error': 'Koji output parsing failed: %s' % line}
+            return {'error': '%s output parsing failed: %s' % (self.command,
+                                                               line)}
         return parse_nvr(cols[0], pkg_name)
 
     def _get_version(self, pkg_name, branch):
         ver = self._koji_get_version(pkg_name, branch)
-        next_branch = "%s-updates-candidate" % branch
+        if self.command == 'brew':
+            next_branch = "%s-candidate" % branch
+        else:
+            next_branch = "%s-updates-candidate" % branch
         next_ver = self._koji_get_version(pkg_name, next_branch)
         if 'version' in next_ver:
             ver['next'] = next_ver
