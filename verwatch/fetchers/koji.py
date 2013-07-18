b727d52604488cc987f1bec1190fabbc7e31612a
Jakub Ruzicka <jruzicka@redhat.com>
2013-07-18 22:12:47 +0200

    Make koji fetcher look for next version harder.

--- verwatch/fetchers/koji.py
+++ verwatch/fetchers/koji.py
@@ -43,11 +43,11 @@ class KojiFetcher(VersionFetcher):
 
     def _get_version(self, pkg_name, branch):
         ver = self._koji_get_version(pkg_name, branch)
-        if self.command == 'brew':
-            next_branch = "%s-candidate" % branch
-        else:
-            next_branch = "%s-updates-candidate" % branch
+        next_branch = "%s-candidate" % branch
         next_ver = self._koji_get_version(pkg_name, next_branch)
+        if 'version' not in next_ver:
+            next_branch = "%s-updates-candidate" % branch
+            next_ver = self._koji_get_version(pkg_name, next_branch)
         if 'version' in next_ver:
             ver['next'] = next_ver
         return ver
