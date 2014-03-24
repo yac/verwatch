7eac231e9cd8ceeb36c14a5edb929f4fca90bfc7
Fabian Deutsch <fabiand@fedoraproject.org>
2014-03-24 09:58:56 +0100

    Add a cnucnu based fetcher
    The cnucnu based fetcher can pull versions frm any HTTP Url.
    The URL can be passed to the fetcher using the branch name.
    
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- verwatch/builtin_fetchers.py
+++ verwatch/builtin_fetchers.py
@@ -4,6 +4,10 @@ import fetchers.git
 import fetchers.koji
 import fetchers.repoquery
 try:
+    import fetchers.cnu
+except ImportError:
+    print "Failed to import cnucnu fetcher. cnucnu module missing?"
+try:
     import fetchers.distgit
 except ImportError:
     # TODO: Something more sofisticated would be nice.
